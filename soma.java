47c45fc8dfb0570c298f15a7a6bb219800bdb790
thomazfalcao <86261484+thomazfalcao@users.noreply.github.com>
2021-06-21 16:37:43 -0300

    Update soma.java

--- soma.java
+++ soma.java
@@ -1,10 +1,10 @@
 
 public class Q3 {
      public static void main(String[] args){
-         int Soma = 0;
+         int soma = 0;
          for (int i = 1;i < 1000; i++){
-             Soma += i;
+             soma += i;
          }
-         System.out.println("Soma Total de 1 até 999:" + Soma);
+         System.out.println("Soma Total de 1 até 999:" + soma);
      }
 }
