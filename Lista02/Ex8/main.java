54520bd3b748b340ca93e6846a169412ed517f1a
thomazfalcao <86261484+thomazfalcao@users.noreply.github.com>
2021-06-28 15:08:37 -0300

    Create main.java

---
+++ Lista02/Ex8/main.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        Celular celular = new Celular();
+        
+        celular.painel();
+        celular.mensagem();
+        celular.mensagem();
+        celular.mensagem();
+        celular.mensagem();
+        celular.painel();
+        celular.ligacao();
+        celular.painel();
+        celular.recarga();
+        celular.painel();
+    }   
+}
