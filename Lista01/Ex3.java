3b6e952d561bd951658bc25e67c32a5c789e7351
thomazfalcao <86261484+thomazfalcao@users.noreply.github.com>
2021-06-28 14:45:17 -0300

    Create Ex3.java

---
+++ Lista01/Ex3.java
@@ -0,0 +1,9 @@
+public class Q3 {
+     public static void main(String[] args){
+         int soma = 0;
+         for (int i = 1;i < 1000; i++){
+             soma += i;
+         }
+         System.out.println("Soma Total de 1 até 999:" + soma);
+     }
+}
