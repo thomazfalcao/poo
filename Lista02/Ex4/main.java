b697dcc95c21ff68a6a8b1a6998d863a04fe6c61
thomazfalcao <86261484+thomazfalcao@users.noreply.github.com>
2021-06-28 14:58:49 -0300

    Create main.java

---
+++ Lista02/Ex4/main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args){
+        Casa casa = new Casa();
+        casa.pintarCasa("Amarela");
+        casa.imprimirCorDaCasa();
+
+        Porta portaDaSala = new Porta();
+        portaDaSala.pintarPorta("Marrom");
+        portaDaSala.alterarDimensoes(0.6f, 1.8f);
+        
+        Porta portaDaCozinha = new Porta();
+        portaDaCozinha.pintarPorta("Preta");
+        portaDaCozinha.alterarDimensoes(0.5f, 1.9f);
+
+        Porta portaDoBanheiro = new Porta();
+        portaDoBanheiro.pintarPorta("Branca");
+        portaDoBanheiro.alterarDimensoes(0.4f, 1.8f);
+
+        casa.addPorta(portaDaSala);
+        casa.addPorta(portaDaCozinha);
+        casa.addPorta(portaDoBanheiro);
+
+        System.out.println("Após abrir 3 portas...");
+        portaDaSala.abrePorta();
+        portaDoBanheiro.abrePorta();
+        portaDaCozinha.abrePorta();
+        casa.quantasPortasEstaoAbertas();
+
+        System.out.println("Após fechar 1 das portas...");
+        portaDoBanheiro.fechaPorta();
+        casa.quantasPortasEstaoAbertas();
+    }
+}
