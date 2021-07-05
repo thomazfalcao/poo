9603aef4a4ecd403f42538b56c4325d762ee8ac2
thomazfalcao <86261484+thomazfalcao@users.noreply.github.com>
2021-07-05 09:43:24 -0300

    Create Inscricao.java

---
+++ Lista03/Ex2/Inscricao.java
@@ -0,0 +1,5 @@
+public class Inscricao {
+    public void inscreverAlunoNaDisciplina(Aluno aluno, Disciplina disciplina){}
+
+    public void inscreverTurmasEmDisciplina(Turma turma, Disciplina disciplina){}
+}
