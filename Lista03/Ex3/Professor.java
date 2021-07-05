9ec63fc38dce290006ec006e0b018b5633e57803
thomazfalcao <86261484+thomazfalcao@users.noreply.github.com>
2021-07-05 10:38:10 -0300

    Create Professor.java

---
+++ Lista03/Ex3/Professor.java
@@ -0,0 +1,7 @@
+import java.util.ArrayList;
+
+public class Professor {
+    private String nome;
+    private String departamento;
+    private ArrayList<Disciplina> disciplinas_leciona = new ArrayList();
+}
