84a1e7a6c27bce92a365ca58bc90eb3b0a52b264
thomazfalcao <86261484+thomazfalcao@users.noreply.github.com>
2021-07-18 19:46:06 -0300

    Create Habilidade.java

---
+++ Lista05/Ex4/Habilidade.java
@@ -0,0 +1,9 @@
+public class Habilidade{
+    private String nome;
+    protected String efeito;
+
+    public Habilidade(String nome, String efeito) {
+        this.nome = nome;
+        this.efeito = efeito;
+    }
+}
