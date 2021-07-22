b786733971056bdbeb6202cbff46c15a38b3db11
thomazfalcao <86261484+thomazfalcao@users.noreply.github.com>
2021-07-22 20:39:37 -0300

    Delete lista06/Ex7 directory

--- lista06/Ex7/Main.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        FuncionarioArquivoBinario.criaArquivoBinario("funcionario1");
-        FuncionarioArquivoBinario.leArquivoBinario("C:\\Users\\lucas\\Documents\\ArquivosTeste\\funcionario1.dat");
-    }
-}
