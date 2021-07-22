57ca42f41d11089844e946f7c7379572bebe3b96
thomazfalcao <86261484+thomazfalcao@users.noreply.github.com>
2021-07-22 20:32:14 -0300

    Create Casa.java

---
+++ lista06/Ex2eEx3/Casa.java
@@ -0,0 +1,5 @@
+public class Casa extends Construcao{
+    public Casa(String identificador, int n_pessoas, int n_lampadas, boolean usaArCondicionado, boolean usaEnergiaRenovavel) {
+        super(identificador, n_pessoas, n_lampadas, usaArCondicionado, usaEnergiaRenovavel);
+    }
+}
