bea365c58ff1a8d12fe0fbdd188e182e4a61bcab
thomazfalcao <86261484+thomazfalcao@users.noreply.github.com>
2021-07-22 20:32:56 -0300

    Create Escola.java

---
+++ lista06/Ex2eEx3/Escola.java
@@ -0,0 +1,5 @@
+public class Escola extends Construcao{
+    public Escola(String identificador, int n_pessoas, int n_lampadas, boolean usaArCondicionado, boolean usaEnergiaRenovavel) {
+        super(identificador, n_pessoas, n_lampadas, usaArCondicionado, usaEnergiaRenovavel);
+    }
+}
