1cb990cbfd4000a9074b2f5ca927f80669985433
thomazfalcao <86261484+thomazfalcao@users.noreply.github.com>
2021-07-22 20:36:38 -0300

    Create Registros.java

---
+++ lista06/Ex5/Registros.java
@@ -0,0 +1,10 @@
+public interface Registros {
+    double totalizarReceita();
+
+    String getProduto();
+    String getDescricao();
+    double getPrecoUnitario();
+    double getPrecoHora();
+    int getHoras();
+    int getQuantidade();
+}
