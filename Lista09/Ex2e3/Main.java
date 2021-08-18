7f7debe322cccd3028f92b0c90870050edb9fef9
thomazfalcao <86261484+thomazfalcao@users.noreply.github.com>
2021-08-17 22:44:05 -0300

    Create Main.java

---
+++ Lista09/Ex2e3/Main.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Q7_Main {
+    public static void main(String[] args) throws IOException {
+        FuncionarioArquivoBinario.criaArquivoBinario("funcionario1");
+        FuncionarioArquivoBinario.leArquivoBinario("C:\\Users\\lucas\\Documents\\ArquivosTeste\\funcionario1.dat");
+    }
+}
